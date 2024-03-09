70c41ed756e11e3b701d8b0df9d9c688b43047fc
paxelotl <141200190+paxelotl@users.noreply.github.com>
2024-03-09 12:50:04 -0800

    Remove ability to jump

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -33,10 +33,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
